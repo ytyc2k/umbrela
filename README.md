e27501d7b0053964b24ca7a52cceda7144d20ffd
ytyc2k@gmail.com <ytyc2k@gmail.com>
2019-05-23 11:32:48 -0500

    changes in 20190523_4

---
+++ README.md
@@ -0,0 +1,8 @@
+# umbrela
+Smart Home:
+To build a Openhab Clould Server with one key.
+Just creat a setup.sh file in Ubuntu Linux Enviroment,patse the contents of the setup.sh in github.
+Run it! You will get a Openhab Clould Server.
+$ cd
+$ chmod +x setup.sh
+$ ./setup.sh
--- setup.sh
+++ setup.sh
@@ -1,18 +1,21 @@
 sudo ln -fs /usr/share/zoneinfo/Canada/Central /etc/localtime
 
-sudo apt-get update && sudo apt-get upgrade
+sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install -y build-essential redis-server mongodb nginx python git
 
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt-get -y install nodejs
 
 cd
+git clone https://github.com/ytyc2k/umbrela.git
 git clone https://github.com/openhab/openhab-cloud.git
 
 cd openhab-cloud
 
-cp ~/config.json ./config.json
-cp ~/user.js ./models/user.js
+cp ~/umbrela/config.json ./config.json
+cp ~/umbrela/user.js ./models/user.js
+cp ~/umbrela/mailer.js ./mailer.js
+
 npm install
 npm update
 npm install bcrypt time
@@ -26,14 +29,11 @@ sudo add-apt-repository ppa:certbot/certbot -y
 sudo apt-get update
 sudo apt-get install -y certbot python-certbot-nginx
 sudo certbot --nginx -d myopenhab.umbrela.co -m smartsnsz@gmail.com -n --agree-tos
-# smartsnsz@gmail.com
-# (A)gree/(C)ancel: A
-# (Y)es/(N)o: N
-# myopenhab.umbrela.co
-# Select the appropriate number [1-2] then [enter] (press 'c' to cancel): 2
 
-sudo cp ~/default /etc/nginx/sites-available/default
+sudo cp ~/umbrela/default /etc/nginx/sites-available/default
+
 cd ~/openhab-cloud
+
 sudo node app.js
 sudo service nginx restart
 
@@ -42,12 +42,5 @@ sudo service nginx restart
 
 # $ sudo crontab -l
 # * 7,19 * * * certbot -q renew
-
-# fix "forgot password" by Email change
-# change mailer.js 
-# before:
-# productionEnv = process.env.NODE_ENV || 'dev';
-# after:
-# productionEnv = process.env.NODE_ENV || 'production';
 
 # show in browser https://myopenhab.umbrela.co
